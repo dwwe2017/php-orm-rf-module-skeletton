ef55892a253b1d89750acf46b0f452a7bb9459f9
dwwe2017 <daniel@dwwe.de>
2019-11-18 23:00:12 +0100

    Adapted to current architecture of the TSI2;

--- Module/fronted/IndexController/indexAction/routes.js
+++ Module/fronted/IndexController/indexAction/routes.js
@@ -1,25 +1,28 @@
 // Copyright 2019. DW </> Web-Engineering. All rights reserved.
 
-import React from "react";
-import {Link, Route, Switch} from "react-router-dom";
+import React, {Fragment} from "react";
+import {Route, Switch} from "react-router-dom";
+import NavItem from "../../NavItem";
 import IndexAction from "./IndexAction";
 import TestAction from "./TestAction";
 
-const Routes = ({ ...props }) => (
-    <div>
-        <ul>
-            <li>
-                <Link to="/">IndexAction</Link>
-            </li>
-            <li>
-                <Link to="/test">TestAction</Link>
-            </li>
-        </ul>
-        <Switch>
-            <Route exact path="/" render={(routeProps) => <IndexAction route={routeProps} {...props} />} />
-            <Route path="/test" render={(routeProps) => <TestAction route={routeProps} {...props} />} />
-        </Switch>
-    </div>
+const Routes = ({...props}) => (
+    <div className="tabbable tabbable-custom tabbable-full-width" >
+        <ul className="nav nav-tabs" >
+            <NavItem to={"/"} >
+                <Fragment >IndexAction</Fragment >
+            </NavItem >
+            <NavItem to={"/test"} >
+                <Fragment >TestAction</Fragment >
+            </NavItem >
+        </ul >
+        <div className="tab-content row" >
+            <Switch >
+                <Route exact path="/" render={(routeProps) => <IndexAction route={routeProps} {...props} />} />
+                <Route path="/test" render={(routeProps) => <TestAction route={routeProps} {...props} />} />
+            </Switch >
+        </div >
+    </div >
 );
 
 export default Routes;
